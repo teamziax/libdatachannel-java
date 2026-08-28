7120fd306825c9137555fff155a5b4e98f0311b5
pschichbot <phillip.github-bot@schich.tel>
2026-08-28 14:05:20 +0000

    Update all dependencies

--- jni/conanfile.py
+++ jni/conanfile.py
@@ -9,7 +9,7 @@ statically_linked_platforms = {
 
 class LibDataChannel(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    requires = "openssl/3.6.3"
+    requires = "openssl/4.0.2"
     generators = "CMakeDeps"
 
     def configure(self):
